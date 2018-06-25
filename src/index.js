67d854e1a8148ce28831e1bae5a358c9508bdc13
Mel Michael <mel.michael@andela.com>
2018-06-25 20:06:39 +0100

    Complete Todo task

--- src/index.js
+++ src/index.js
@@ -1,36 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import todoApp from "./reducers";
+import App from "./App";
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { createStore } from "redux";
-import todoApp from "./reducers";
-// import {
-//   addTodo,
-//   toggleTodo,
-//   setVisibilityFilter,
-//   VisibilityFilters
-// } from "./actions";
-
 const store = createStore(todoApp);
-// Log the initial state
-console.log(store.getState());
 
-// Every time the state changes, log it
-// Note that subscribe() returns a function for unregistering the listener
-// const unsubscribe = store.subscribe(() => console.log(store.getState()));
-
-// Dispatch some actions
-// store.dispatch(addTodo("Learn about actions"));
-// store.dispatch(addTodo("Learn about reducers"));
-// store.dispatch(addTodo("Learn about store"));
-// store.dispatch(toggleTodo(0));
-// store.dispatch(toggleTodo(1));
-// store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
-
-// Stop listening to state updates
-// unsubscribe();
-
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
+
+
